3e88f9e034c587bba961263320e4d400dfec7cd2
shakeeb.raza <sja@eclear.com>
2023-10-17 13:59:42 +0200

    initial commit

--- src/main/java/com/example/demo/GreetController.java
+++ src/main/java/com/example/demo/GreetController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 import com.example.demo.api.GreetApi;
 import com.example.demo.model.SampleMsg;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,14 +19,30 @@ public class GreetController implements GreetApi {
 
     @Override
     public ResponseEntity<SampleMsg> findGreetByStatus(String status) {
-        SampleMsg sampleMsg=new SampleMsg();
-        sampleMsg.message("hello, "+status);
+        SampleMsg sampleMsg = new SampleMsg();
+        sampleMsg.message("hello, " + status);
         return ResponseEntity.ok(sampleMsg);
     }
 
     @Override
     public ResponseEntity<SampleMsg> getCall() {
-        SampleMsg sampleMsg=new SampleMsg();
+        SampleMsg sampleMsg = new SampleMsg();
+        sampleMsg.message("hello, World");
+        return ResponseEntity.ok(sampleMsg);
+    }
+
+    @Override
+    public ResponseEntity<SampleMsg> postCall(SampleMsg sampleMsg) {
+        sampleMsg.message("hello ".concat(sampleMsg.getMessage()));
+        return ResponseEntity.ok(sampleMsg);
+    }
+
+    @Override
+    public ResponseEntity<SampleMsg> findGreetByStatusError(String status) {
+        if (status == null || status.isEmpty() || status.equals( "null")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new SampleMsg().message("Error"));
+        }
+        SampleMsg sampleMsg = new SampleMsg();
         sampleMsg.message("hello, World");
         return ResponseEntity.ok(sampleMsg);
     }
